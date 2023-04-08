4e3c2f0d9c0dae46c9a7215167d2266441b44d5b
Durvesh7k <dschopade7k@gmail.com>
2023-04-09 00:54:05 +0530

    first commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+
+const {ethers} = require('hardhat');
+
+async function main() {
+  const deployer = await ethers.getSigners()
+  const Contract = await ethers.getContractFactory('NFTMarketplace');
+  const marketplaceContract = await Contract.deploy();
+  await marketplaceContract.deployed();
+
+  console.log("The contract is deployed on the address: ",marketplaceContract.address);
+  console.log("The contract is deployed by: ",deployer.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
